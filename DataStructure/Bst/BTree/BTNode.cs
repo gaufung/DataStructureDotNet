d5b93b99b4752b4fc808794ed42afac444c338a3
gaufung <gaufung@foxmail.com>
2015-04-16 16:48:05 +0800

    bst adt definiton

---
+++ DataStructure/Bst/BTree/BTNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bst.BTree
+{
+    /// <summary>
+    /// 超级节点的定义
+    /// </summary>
+    /// <typeparam name="T">关键码</typeparam>
+    public class BtNode<T> where T:IComparable
+    {
+        public BtNode<T> Parent { get; set; }
+        public List<T> Key { get; set; }
+        public List<BtNode<T>> Child { get; set; }
+
+        public BtNode()
+        {
+            Parent = null;
+            Child.Insert(0,null);
+        }
+        public BtNode(T e, BtNode<T> lc = null, BtNode<T> rc = null)
+        {
+            Parent = null;
+            Key.Insert(0,e);
+            Child.Insert(0,lc);
+            Child.Insert(1,rc);           
+            if (lc!=null)
+            {
+                lc.Parent=this;
+            }
+            if(rc!=null)
+            { 
+                rc.Parent=this;
+            }
+        } 
+    }
+}
---
+++ DataStructure/Bst/BTree/BTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Bst.BTree
+{
+    public class BTree<T> where T :IComparable
+    {
+        public int Size { get; private set; }
+        private readonly int _order;
+        public BtNode<T> Root { get; private set; }
+        public BtNode<T> Hot { get; private  set; }
+
+        public bool Empty
+        {
+            get
+            {
+                return Root == null;
+            }
+        }
+
+        protected void SolveOverFlow(BtNode<T> x)
+        {
+            throw new NotImplementedException();
+            
+        }
+
+        protected void SolveUnderFlow(BtNode<T> x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BTree(int order = 3)
+        {
+            _order = 3;
+            Size = 0;
+            Root=new BtNode<T>();
+        }
+
+        public BtNode<T> Search(T e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(T e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
