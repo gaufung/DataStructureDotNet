ed3455eb8986364145ebceb6666967db91588fc5
gaufung <gaufung@outlook.com>
2016-05-27 16:27:53 +0800

    modify huffman

--- DataStructure/BinaryTreeTest/HuffmanTreeTest.cs
+++ DataStructure/BinaryTreeTest/HuffmanTreeTest.cs
@@ -7,26 +7,9 @@ namespace BinaryTreeTest
     [TestFixture]
     public class HuffmanTreeTest
     {
+       
         [Test]
-        public void Test()
-        {
-            //IVector<char> nodes=Vector<char>.VectorFactory();
-            //nodes.Insert(0, 'a');
-            //nodes.Insert(0, 'b');
-            //nodes.Insert(0, 'c');
-            //nodes.Insert(0, 'd');
-            //IVector<double> weights=Vector<double>.VectorFactory();
-            //weights.Insert(0, 7.0);
-            //weights.Insert(0, 5.0);
-            //weights.Insert(0, 2.0);
-            //weights.Insert(0, 4.0);
-            //BinNode<char> root=HuffmanTree<Char>.BuildHuffmanTree(nodes, weights);
-            //root.TravLevel(item => Trace.Write(item));
-
-        }
-
-        [Test]
-        public void TestNew()
+        public void TestHuffaman()
         {
             char[] chars = { 'a', 'b', 'c', 'd' };
             double[] weight = { 7.0, 5.0, 2.0, 4.0 };
